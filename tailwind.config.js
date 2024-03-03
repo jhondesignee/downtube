357382ffcca44c3d3278a74c4fc3a8d9a76892fb
Jhonata ¦ dev <jhondesignee@gmail.com>
2024-03-03 06:34:47 -0300

    Revert "Create interface"

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin")
-
-module.exports = {
-  content: ["./app/**/*.tsx", "./themes/**/*.tsx", "./components/**/*.tsx"]
-}
